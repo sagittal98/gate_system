8f6ea970471be499a56832850f97d9438c837173
xslg <25169559166@qq.com>
2020-12-25 14:10:31 +0800

    用户

---
+++ src/test/com/jyyd/gate/service/impl/UserLoginServiceImplTest.java
@@ -0,0 +1,34 @@
+package com.jyyd.gate.service.impl;
+
+import com.jyyd.gate.model.Result;
+import com.jyyd.gate.model.UserModel;
+import com.jyyd.gate.service.UserLoginService;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * @Author:yangbiao
+ * @Describe
+ * @Create 2020-12-25
+ */
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(locations = {"classpath*:spring/*.xml"})
+class UserLoginServiceImplTest {
+    @Autowired
+    private UserLoginService userLoginService;
+
+    @Test
+    void testSelectUser() {
+
+        UserModel userModel = new UserModel("超级管理员", "1234568");
+        Result<Boolean> booleanResult = userLoginService.selectUser(userModel);
+        System.out.println(booleanResult);
+    }
+}
